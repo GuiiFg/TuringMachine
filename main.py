cd442da34e04050bf349d9dac27bbf3d14b55ff8
Guilherme <guilherme@wemov.com.br>
2023-05-10 18:57:45 -0300

    inventendo o automato -> turing para turing -> automato

--- main.py
+++ main.py
@@ -2,30 +2,41 @@ import TuringMachine
 
 def BinaryComplementTest():
   fitasDeTeste = [
-    ['<', '1', '0', '1'],
-    ['<', '0', '1', '1'],
-    ['<', '1', '0', '1', '0', '1', '0'],
-    ['<', '0', '0', '1', '1', '1'],
+    ['<', 1, 0, 1],
+    ['<', 0, 1, 1],
+    ['<', 1, 0, 1, 0, 1, 0],
+    ['<', 0, 0, 1, 1, 1],
   ]
 
   fitasDeResultado = [
-    ['<', '0', '1', '0'],
-    ['<', '1', '0', '0'],
-    ['<', '0', '1', '0', '1', '0', '1'],
-    ['<', '1', '1', '0', '0', '0'],
+    ['<', 0, 1, 0],
+    ['<', 1, 0, 0],
+    ['<', 0, 1, 0, 1, 0, 1],
+    ['<', 1, 1, 0, 0, 0],
   ]
 
   for i in range(len(fitasDeTeste)):
-    automaton = TuringMachine.BinaryComplementAutomaton()
-
     turing_machine = TuringMachine.TuringMachine()
     turing_machine.coil = fitasDeTeste[i]
 
-    automaton.Start(turing_machine)
+    turing_machine.automatons['BinaryComplement'] = TuringMachine.Automatons.BinaryComplementAutomaton()
 
-    if automaton.isFinalState:
+    turing_machine.run('BinaryComplement')
+
+    if turing_machine.automatons['BinaryComplement'].isFinalState:
       print(f'Test {i + 1} - Success!')
     else:
       print(f'Test {i + 1} - Error!')
 
 BinaryComplementTest()
+
+import TuringMachine
+
+turing_machine = TuringMachine.TuringMachine()
+turing_machine.coil = ['<', 1, 0, 1]
+
+turing_machine.automatons['BinaryComplement'] = TuringMachine.Automatons.BinaryComplementAutomaton()
+turing_machine.automatons['BinaryFourMultiply'] = TuringMachine.Automatons.BinaryMultiplyFourAutomaton()
+
+turing_machine.run('BinaryComplement')
+turing_machine.run('BinaryFourMultiply')
