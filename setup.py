d3abbe0f9f973eaef26711321be97548a546c2c1
Guilherme <Farias._02f@outlook.com>
2023-05-10 22:19:43 -0300

    up setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import TuringMachine
+from distutils.core import setup
+
+setup(name='gf_TuringMachine',
+      version='1.0',
+      description='Package for work with automatons and turing machine',
+      author='Guilherme Farias Garcia',
+      author_email='farias._02f@outlook.com',
+      url='https://github.com/GuiiFg/TuringMachine',
+      packages=['TuringMachine'],
+     )
