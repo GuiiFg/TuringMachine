ca7efffb6b68594128da42fb3de2d2ef255fcd1c
Guilherme <Farias._02f@outlook.com>
2023-05-11 19:56:35 -0300

    running setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='gf_TuringMachine',
-      version='1.0',
+      version='1.1',
       description='Package for work with automatons and turing machine',
       author='Guilherme Farias Garcia',
       author_email='farias._02f@outlook.com',
